39a3b3291c410ecf56425a65569cf6537313215d
ZiglaCity <ziglacity@gmail.com>
2024-11-10 03:09:55 +0000

    New function for converting currencies accurately

--- functions.py
+++ functions.py
@@ -36,6 +36,7 @@ os.makedirs(cache_dir, exist_ok=True)
 app_cache_path = os.path.join(cache_dir, json_file_path)
 
 def update_exchange_rate():
+    print("Updating exchange rates")
 
     try:
         # Fetch the current exchange rates from the API
@@ -74,7 +75,7 @@ def update_exchange_rate():
         # extract the exchange rate from the cache file if user is offline
         try:
             with open(app_cache_path, 'r') as file:
-                offline_rate = file.read()
+                offline_rate = json.load(file)
                 if offline_rate:
                     exchange_rate_offline_data = offline_rate
                     print(f"No Connction so this is the offline exchange rate {exchange_rate_offline_data}")
@@ -82,4 +83,55 @@ def update_exchange_rate():
             print("No Connection and File Not Found")
             return
 
-update_exchange_rate()
+# DEBUG
+# update_exchange_rate()
+
+
+#define a function to convert the curries appropriately
+def convert_currency():
+    global entry_top, to_currency,answer, from_currency, entry_bottom, answer, app_cache_path 
+    if not entry_top.get():
+        amount = 0
+    else:
+        amount = float(entry_top.get())
+    to_currency = to_
+    from_currency = from_
+
+    #fetch the latest rates
+    try:
+        with open(app_cache_path, 'r') as file:
+            offline_rate = json.load(file)
+            if offline_rate:
+                rates = offline_rate
+                exchange_rate_offline_data = rates
+                print(f"this is the offline exchange rate {exchange_rate_offline_data}")
+            else:
+                print("File Empty")
+                rates = exchange_rate_offline_data
+    except (FileNotFoundError, PermissionError, IsADirectoryError, OSError):
+        print("File Not Found while converting")
+        rates = exchange_rate_data
+
+    #do the convertion
+    if from_currency == "USD":
+        converted_amount = float(amount) * float(rates[to_currency])
+        
+    else:
+        converted_amount = float(amount) / float(rates[from_currency]) * float(rates[to_currency])
+        entry_bottom.config(state="normal")
+        
+    #round the converted value to an appropriate decimal places
+    if converted_amount > 1:
+        converted_amount = round(converted_amount, 2)
+    elif converted_amount > 0.01:
+        converted_amount = round(converted_amount, 3)
+    else:
+        converted_amount = round(converted_amount, 5)
+
+    #now fix the anser in the down entry and change its mode to read only
+    answer.set(converted_amount)
+    entry_bottom.config(state="readonly")
+
+    return converted_amount
+
+
